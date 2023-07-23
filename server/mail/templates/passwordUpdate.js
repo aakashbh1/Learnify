c201007352673a5e5863e5535db9d0128a33f90b
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-07-23 17:20:57 +0530

    Update passwordUpdate.js

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -55,7 +55,7 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
+            <a href="https://learnifyedtech.vercel.app/"><img class="logo"
                     src="https://i.postimg.cc/CKbBR363/Learnify-2-a.png" alt="Learnify Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
