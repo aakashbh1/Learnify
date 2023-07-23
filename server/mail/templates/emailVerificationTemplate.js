f3b2e94862dfd14855603bfa62cbf44077a5292d
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-07-23 17:20:43 +0530

    Update emailVerificationTemplate.js

--- server/mail/templates/emailVerificationTemplate.js
+++ server/mail/templates/emailVerificationTemplate.js
@@ -66,7 +66,7 @@ const otpTemplate = (otp) => {
 	
 	<body>
 		<div class="container">
-			<a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
+			<a href="https://learnifyedtech.vercel.app/"><img class="logo"
 					src="https://i.postimg.cc/CKbBR363/Learnify-2-a.png" alt="Learnify Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
