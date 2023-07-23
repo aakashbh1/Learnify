c54806ef8d87e7e79f5755c03aab15128560d395
Aakash Bhargava <92159539+aakashbh1@users.noreply.github.com>
2023-07-23 17:21:11 +0530

    Update paymentSuccessEmail.js

--- server/mail/templates/paymentSuccessEmail.js
+++ server/mail/templates/paymentSuccessEmail.js
@@ -67,7 +67,7 @@ exports.paymentSuccessEmail = (name, amount, orderId, paymentId) => {
       
       <body>
           <div class="container">
-              <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.postimg.cc/CKbBR363/Learnify-2-a.png"
+              <a href="https://learnifyedtech.vercel.app/"><img class="logo" src="https://i.postimg.cc/CKbBR363/Learnify-2-a.png"
                       alt="Learnify Logo"></a>
               <div class="message">Course Payment Confirmation</div>
               <div class="body">
